538a64c2cdbfc87d64f4d3c764730137f66a85be
niole <niolenelson@gmail.com>
2022-03-29 21:43:36 +0000

    Fix some dumb crap

--- amazon-linux/install.sh
+++ amazon-linux/install.sh
@@ -2,9 +2,9 @@
 
 # assumes that tmux is 3.2a
 
-PYTHON_2_NODE_HOST="/usr/bin/python3"
-PYTHON_3_NODE_HOST="/usr/bin/python3"
-NEOVIM_NODE_HOST="/home/linuxbrew/.linuxbrew/bin/neovim-node-host"
+PYTHON_2_NODE_HOST="\/usr\/bin\/python3"
+PYTHON_3_NODE_HOST="\/usr\/bin\/python3"
+NEOVIM_NODE_HOST="\/home\/linuxbrew\/.linuxbrew\/bin\/neovim-node-host"
 
 sed "s/PYTHON_2_NODE_HOST/$PYTHON_2_NODE_HOST/g" .vimrc | \
     sed "s/PYTHON_3_NODE_HOST/$PYTHON_3_NODE_HOST/g" | \
@@ -12,12 +12,17 @@ sed "s/PYTHON_2_NODE_HOST/$PYTHON_2_NODE_HOST/g" .vimrc | \
 
 cp .tmux.conf ~/
 
-npm install -g neovim-node-host
-sudo yum install neovim
+# neovim
+npm install -g neovim # for node host
+sudo yum install neovim # for neovim
+# neovim
+
+# vim plug
+mkdir -p ~/.vim/autoload/
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-brew install ripgrep
+# vim plug
+
+brew install ripgrep fzf
 
 nvim +PlugInstall +qall
-
-
