686f084c386d041194c76e32c0c629c54ed6f977
niole <niolenelson@gmail.com>
2023-10-05 20:01:10 -0700

    Add dir for ubuntu

---
+++ ubuntu/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# assumes that tmux is 3.2a
+
+PYTHON_2_NODE_HOST="\/usr\/bin\/python3"
+PYTHON_3_NODE_HOST="\/usr\/bin\/python3"
+NEOVIM_NODE_HOST="\/home\/linuxbrew\/.linuxbrew\/bin\/neovim-node-host"
+
+sed "s/PYTHON_2_NODE_HOST/$PYTHON_2_NODE_HOST/g" .vimrc | \
+    sed "s/PYTHON_3_NODE_HOST/$PYTHON_3_NODE_HOST/g" | \
+    sed "s/NEOVIM_NODE_HOST/$NEOVIM_NODE_HOST/g" > ~/.vimrc
+
+cp .tmux.conf ~/
+
+cp git.plugin.sh ~/
+
+sudo apt install curl vim neovim ripgrep fzf gh tmux
+
+# neovim
+npm install -g neovim # for node host
+# neovim
+
+# vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim plug
+
+vim +PlugInstall +qall
+
+git config --global user.email "niolenelson@gmail.com"
+git config --global user.name "niole"
+
+echo "Please run the following command in vim in order to setup copilot. See https://github.com/github/copilot.vim for other Qs."
+echo ":Copilot setup"
+echo "add source ~/git.plugin.sh to .bashrc"
